c7de960a7d3dd203aa2b31df9610514b3a1fc9e9
Francis Obiagwu <cyber.francis@comcast.net>
2023-12-31 16:45:08 -0600

    chore: added unittests

---
+++ tests/test_structured_logger.py
@@ -0,0 +1,5 @@
+from structlog_telemetry.structured_logger import StructuredLogger
+
+def test_structured_logger():
+    logger = StructuredLogger(app_name="simple_logger", app_version="v0.0.1")
+    logger.info({"EVENT": {"TYPE": "ACCESS", "NAME": "USER_NOT_FOUND"}})
