3d733e3b0bbdbf9ac7e066021c55be857054a324
Francis Obiagwu <cyber.francis@comcast.net>
2024-06-04 05:40:12 -0500

    fix: updated banner

--- src/structlog_telemetry/singleton_base.py
+++ src/structlog_telemetry/singleton_base.py
@@ -1,6 +1,3 @@
-from .banner import print_banner
-from .version import __version__
-
 
 class Singleton(object):
     _instance = None
@@ -8,6 +5,5 @@ class Singleton(object):
     def __new__(cls, *args, **kwargs) -> dict:
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            print_banner(cls.__name__, __version__)
 
         return cls._instance
